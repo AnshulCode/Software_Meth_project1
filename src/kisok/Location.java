626a358b2ebba00f26e65242542cf5e44b6fed99
AnshulCode <kaarkid1@gmail.com>
2022-01-28 20:56:49 -0500

    fixed location enum class

--- src/kisok/Location.java
+++ src/kisok/Location.java
@@ -1,9 +1,15 @@
 package kisok;
 
 public enum Location {
-    MORRIS,
-    MIDDLESEX,
-    SOMMERSET,
-    UNION,
-    MERCER
+    MORRIS("07960","Morristown"),
+    MIDDLESEX("08852","Piscataway"),
+    SOMMERSET("08807","Bridgewater"),
+    UNION("07083","Union"),
+    MERCER("08542","Princeton");
+    public final String zip;
+    public final String cites;
+    private Location(String zipcode,String cites){
+        this.zip = zipcode;
+        this.cites = cites;
+    }
 }
